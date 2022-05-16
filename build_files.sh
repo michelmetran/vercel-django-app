bcb036bd4c56cda35fb92f961569fffc5aae95ac
Michel Metran <michelmetran@gmail.com>
2022-05-15 21:07:51 -0300

    Ajustes

--- build_files.sh
+++ build_files.sh
@@ -2,8 +2,7 @@
 python3.9 -m pip install --upgrade pip
 
 # Install requirements
-#pip install -r requirements.txt
-python3.9 -m pip install --upgrade pip
+pip install -r requirements.txt
 
 # Collect Static Files on Deploy
 python3.9 manage.py collectstatic
